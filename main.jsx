3158fa2a2fe8a6f40d4e6d1f034811e2fb97e6d4
nikstishka-lang <nikstishka@gmail.com>
2025-09-13 12:59:02 +0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import IGCardGenerator from "./IGCardGenerator";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<IGCardGenerator />);
