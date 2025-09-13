47f361e92e87befecc7fac6105caf360f0ce0537
nikstishka-lang <nikstishka@gmail.com>
2025-09-13 12:39:42 +0300

    Add files via upload

---
+++ IGCardGenerator.jsx
@@ -0,0 +1,4 @@
+import React, { useRef, useState } from "react";
+export default function IGCardGenerator() {
+  return <div style={{padding: '50px', textAlign:'center'}}>Ваш генератор карточек здесь</div>;
+}
